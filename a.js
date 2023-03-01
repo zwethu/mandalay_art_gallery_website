2397e1ea776284e1f9a1896af2e322936d990b96
zwethu <thuraung1313@gmail.com>
2023-03-01 23:15:53 +0630

    reverse changes

--- a.js
+++
@@ -1,4 +0,0 @@
-// const {artistData} = require("./data/artists_data.js");
-// const {exhibitionsData} = require("./data/exhibitions_data.js");
-// var a = artistData[1].paintings[1];
-// console.log(a);
